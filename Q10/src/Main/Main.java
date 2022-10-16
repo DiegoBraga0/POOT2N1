4814e4c7b2ac1ec496d9deff83dd3959dee3b9a2
DiegoBraga0 <113713737+DiegoBraga0@users.noreply.github.com>
2022-10-16 16:42:53 -0300

    Update Main.java

--- Q10/src/Main/Main.java
+++ Q10/src/Main/Main.java
@@ -1,10 +1,14 @@
 package Main;
 
+import Tabuada.*;
+
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    
+    public static void main(String[] args) {
+        
+        Tab tb = new Tab();
+        tb.TabuadaNum();
+        
+    }
+    
 }
