22da9acdd4b5fdacaaa6a4c8f1af73f28db1042f
DiegoBraga0 <113713737+DiegoBraga0@users.noreply.github.com>
2022-10-16 16:41:38 -0300

    Update Tab.java

--- Q10/src/Tabuada/Tab.java
+++ Q10/src/Tabuada/Tab.java
@@ -1,5 +1,15 @@
 package Tabuada;
 
 public class Tab {
-
+    
+    public void TabuadaNum(){
+        
+        for(int n = 1; n < 11; n++){
+            System.out.println("\n");
+            System.out.println("Tabuada do " + n + " : \n");
+            for(int i = 1; i < 11; i++){
+                System.out.println(n + " x " + i + " = "+ n*i);
+            }
+        }
+    }  
 }
