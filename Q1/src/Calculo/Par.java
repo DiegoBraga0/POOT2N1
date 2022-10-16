6e8c35dc7b456351f6ef372aec90c150aa2d22b5
DiegoBraga0 <113713737+DiegoBraga0@users.noreply.github.com>
2022-10-16 11:32:05 -0300

    Update Par.java

--- Q1/src/Calculo/Par.java
+++ Q1/src/Calculo/Par.java
@@ -1,5 +1,30 @@
 package Calculo;
 
+import java.util.Scanner;
+
 public class Par {
+    
+    Scanner sc = new Scanner(System.in);
+    
+    public int num;
+    
+    public Par(){	
+        num = 0;
+    }
+    
+    public void Verification(){
+        
+        System.out.println("Digite o número que você deseja verificar : ");
+        
+        num = sc.nextInt();				
+	if(num % 2 == 0) {
+	    System.out.println("Par!");
+	}
+        else{
+	    System.out.println("Impar!");							
+	}
+																								
+    }				
 
 }
+
