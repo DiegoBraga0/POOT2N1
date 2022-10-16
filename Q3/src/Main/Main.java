6a7335ae9d393d16063cf20b1c55425a9a825987
DiegoBraga0 <113713737+DiegoBraga0@users.noreply.github.com>
2022-10-16 12:19:11 -0300

    Update Main.java

--- Q3/src/Main/Main.java
+++ Q3/src/Main/Main.java
@@ -1,10 +1,15 @@
 package Main;
 
+import Variaveis.*;
+
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    
+    public static void main(String[] args) {
+        
+        Var V = new Var();
+        
+        V.baskara();
+       
+    }
+    
 }
